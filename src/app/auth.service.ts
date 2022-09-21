bd74f04bc9d513f689752aef148c1b0fd2e066e3
ThomasTacoVos <tvvos@hotmail.nl>
2022-09-21 14:05:18 +0200

    added bootstrap

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import { User } from './user';
 import { UsersDB } from './db/test-users';
+import { BehaviorSubject, Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  // private currentUserSubject: BehaviorSubject<User>;
+  // public currentUser: Observable<User>;
+
+
+  constructor() { 
+    // this.currentUserSubject = new BehaviorSubject<User>(UsersDB.values[])
+    // this.currentUser = this.currentUserSubject.asObservable();
+
+  }
 
   onSubmit({ username, password }: { username: string; password: string; }){
     for (let user of UsersDB){
